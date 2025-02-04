f6c86ff5da522acfd0c92800c103677c91fa82f2
keshav260820 <keshav.angra.21@gmail.com>
2025-02-04 19:56:39 +0530

    Solution to Check Equal To

---
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int x,y;
+    if(x==y){
+        printf("True");
+    }else{
+        printf("False");
+    }
+}
