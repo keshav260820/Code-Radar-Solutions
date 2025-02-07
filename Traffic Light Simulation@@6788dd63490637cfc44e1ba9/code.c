93cd1ddeb99678a03f825b9a9989e2d78f60a22e
keshav260820 <keshav.angra.21@gmail.com>
2025-02-07 06:54:09 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char x;
+    scanf("%c",&x);
+    switch(x){
+        case 'R':
+        printf("Stop");
+        break;
+        case 'G':
+        printf("Go");
+        break;
+        case 'Y':
+        printf("Slow Down");
+        break;
+    }
+}
